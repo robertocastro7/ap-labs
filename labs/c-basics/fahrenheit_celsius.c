0bc5a164b29e6e770f252e1be52c3ff533d149fc
Oscar Roberto Castro <robertoc@MacBook-Air-2.local>
2021-02-16 20:18:09 -0700

    Solved

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
 #define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
+#define   UPPER  100     /* upper limit */
+#define   STEP   10      /* step size */
 
 /* print Fahrenheit-Celsius table */
 
 int main()
 {
-    int fahr;
+    int fahr, cel;
+    float l, u, s;
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahr);
+    cel = (5.0/9.0)*(fahr-32);
+    printf("%.2f Celsius = %.2f Fahrenheit", cel, fahr);
+    return 0;
+    
+    printf("Enter the lower limit: ");
+    scanf("%f", &l);
+    printf("Enter the upper limit: ");
+    scanf("%f", &u);
+    printf("Enter the step: ");
+    scanf("%f", &s);
+    for (fahr = l; fahr <= u; fahr = fahr + s)
 	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
     return 0;
