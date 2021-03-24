7239d6b5ae44941a524a350e7bc806bb680a15bf
Oscar Roberto Castro <robertoc@MacBook-Air-2.local>
2021-03-23 19:41:35 -0700

    Solve strlib lab
    Signed-off-by: Oscar Roberto Castro <robertoc@MacBook-Air-2.local>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    if (argc == 4){        
+        char *command=argv[1];
+        char *str1 =argv[2];
+        char *str2=argv[3];
+        if(strcmp(command,"-add") == 0){
+            mystradd(str1,str2);
+        }else if(strcmp(command,"-find") == 0){
+            mystrfind(str1,str2);
+        }
+    }else{
+        printf("Arguments wrong\n");
+        return 0;    
+    }    
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,52 @@
+#include <stdio.h>   
+#include <stdlib.h>
 
 
 int mystrlen(char *str){
-    return 0;
+    int acum = 0;
+    while (str[acum] != NULL){
+        acum++;
+    }
+    return acum;
 }
 
 char *mystradd(char *origin, char *addition){
+    printf("%s\t%d\n","Initial Length:",mystrlen(origin));
+    
+    int i = 0;
+    int newSize = mystrlen(origin)+mystrlen(addition);
+    char newCharacter[newSize];
+
+    for (int i = 0; i < mystrlen(origin); i++){
+        newCharacter[i] = origin[i];
+    }
+    for (int j = mystrlen(origin); j < (mystrlen(origin)+mystrlen(addition)); j++){
+        i++;
+        newCharacter[j] = addition[i];        
+    }
+
+    printf("New String: %s\n", newCharacter);
+    printf("New Lenght: %d\n", mystrlen(newCharacter));
+
     return 0;
 }
 
 int mystrfind(char *origin, char *substr){
+    printf("%s%s%s","['",substr,"']"); 
+    int current = 0;
+    while (*origin != '\0') {
+        while(*origin ==* substr){
+            origin++;
+            substr++;
+            if(*substr == '\0'){
+                printf("%s String was found at [%d] position\n",substr,current); 
+                return 0;
+            }
+        }
+        substr = &substr[0];
+        current++;
+        origin++;
+    }
+    printf("%s\n","String was not found "); 
     return 0;
 }
