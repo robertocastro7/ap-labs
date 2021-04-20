3752be25f2d42b04d6e39e6a486a549a82c89dcc
Oscar Roberto Castro <robertoc@MacBook-Air-2.local>
2021-04-20 18:36:26 -0500

    Solving advanced-logger
    Signed-off-by: Oscar Roberto Castro <robertoc@MacBook-Air-2.local>

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -6,29 +6,29 @@ int main(int argc, char **argv){
 
     printf("%d\n", atoi(argv[1]));
     switch(atoi(argv[1])) {
-    case 1:
-	// default logging
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 2:
-	// stdout logging
-	initLogger("stdout");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 3:
-	// syslog logging
-	initLogger("syslog");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    default:
-	errorf("Invalid test case");
-	break;
+    	case 1:
+			// default logging
+			infof("INFO Message %d", 1);
+			warnf("WARN Message %d", 2);
+			errorf("ERROR Message %d", 2);
+			break;
+  	  	case 2:
+			// stdout logging
+			initLogger("stdout");
+			infof("INFO Message %d", 1);
+			warnf("WARN Message %d", 2);
+			errorf("ERROR Message %d", 2);
+			break;
+    	case 3:
+			// syslog logging
+			initLogger("syslog");
+			infof("INFO Message %d", 1);
+			warnf("WARN Message %d", 2);
+			errorf("ERROR Message %d", 2);
+			break;
+ 		default:
+			errorf("Invalid test case");
+			break;
     }
 
     return 0;
